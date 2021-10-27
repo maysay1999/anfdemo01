41e2f5dcc670551b0712d2a1cea0c4ac83d4c512
Meisei Takemoto (NETAPP INC) <b-mtakemoto@microsoft.com>
2021-10-28 03:20:16 +0900

    CSI installation shell ver 0.1

---
+++ astra/csi-install.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+kubectl apply -f snapshot.storage.k8s.io_volumesnapshotclasses.yaml
+kubectl apply -f snapshot.storage.k8s.io_volumesnapshotcontents.yaml
+kubectl apply -f snapshot.storage.k8s.io_volumesnapshots.yaml
+kubectl apply -f rbac-snapshot-controller.yaml
+kubectl apply -f setup-snapshot-controller.yaml
