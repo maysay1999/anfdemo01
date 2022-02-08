6f7bd0fbea9acc79b0ade40b3f6b2718c015b7ce
Meisei Takemoto (NETAPP INC) <b-mtakemoto@microsoft.com>
2022-02-08 19:23:32 +0900

    remove unnecessary files

--- create_anf_subnet.sh
+++
@@ -1,8 +0,0 @@
-MCRG=MC_anftest-rg_AnfCluster01_japaneast
-MCVNET=aks-vnet-xxxxxxxx
-az network vnet subnet create \
-    --resource-group $MCRG \
-    --vnet-name $MCVNET \
-    --name netapp-subnet \
-    --delegations "Microsoft.NetApp/volumes" \
-    --address-prefixes 10.0.0.0/26
